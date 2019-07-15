0d7696c3c9135e0f5afe706590ed13b4f8e0b8ab
nanotrigueiro <nanotrigueiro@hotmail.com>
2019-07-15 00:03:31 -0300

    Questão dois de logica

--- logica/questao2.pl
+++ logica/questao2.pl
@@ -1,9 +1,24 @@
-% Premissas
-p(deus).
-q(amor).
-q(steve_wonder).
-r(?).
+% Student exercise profile
+:- set_prolog_flag(occurs_check, error).        % disallow cyclic terms
+:- set_prolog_stack(global, limit(8 000 000)).  % limit term space (8Mb)
+:- set_prolog_stack(local,  limit(2 000 000)).  % limit environment space
 
+%a) Premissas
+amor(deus).
+cego(amor).
+cego(steve_wonder).
+deus(?).
+
+%b) Premissas
+dieta(pessoa).
+comev(pessoa).
+comev(elefante).
+comep(pessoa).
+comep(baleia).
+gordo(elefante).
+gordo(baleia).
+
+% Regras
 entao(X,Y) :-
     X,\+ Y -> false;
     true.
@@ -13,52 +28,58 @@ entao(X,Y) :-
 entao(X,Y) equivale a operação Se p, então q.(p -> q)
 
 a)
-  p(x) -> x é amor
-  q(x) -> x é cego
-  .:r(x) -> x é deus
+  amor(x) -> x é amor
+  cego(x) -> x é cego
+  .:deus(x) -> x é deus
   
-  (1) p(x) - proposição
-  (2) q(x) - proposição
-  (3) p(x) ^ q(x) - conjunção em (1) e (2)
+  (1) amor(x) - proposição
+  (2) cego(x) - proposição
+  (3) amor(x) ^ cego(x) - conjunção em (1) e (2)
   ----------------------------------------
   
-  p(deus) ^ q(amor) ^ q(steve_wonder)
+  amor(deus) ^ cego(amor) ^ cego(steve_wonder)
   ____________________________
-  .: r(steve_wonder)
+  .: deus(steve_wonder)
   
-  Não é possível afirmar esta conclusão, pois p(x)^q(x)^q(x) -> r(x) não é 
+  Não é possível afirmar esta conclusão, pois amor(x)^cego(x)^cego(x) -> deus(x) não é 
   uma tautologia. Portanto este argumento é uma falácia.
   
-  consulta: entao(\+r(steve_wonder), \+ (p(deus);q(amor);q(steve_wonder)))
+  Fazendo uma prova pela contrapositiva(~q -> ~p):
+  consulta: entao(\+deus(steve_wonder), \+ (amor(deus);cego(amor);cego(steve_wonder)))
 
 b)
-  Para todas pessoas p, p quer emagrecer, p faz dieta.
+  Para todas pessoas p, p faz dieta.
   Para toda dieta d, d recomenda o consumo de veduras e peixe.
-  Para todo elefante e baleia eb, eb consome verduras e peixe.
-  eb são gordos.
+  Para todo elefante e, e consome verduras e para toda baleia b, b consome peixe.
+  e e b são gordos.
   .: d engorda.
   
-  p(x) -> x quer emagrecer
-  q(x) -> x faz dieta
-  r(x) -> x recomenda o consume de verduras e peixe
-  s(x) -> x consome verduras e peixe.
-  t(x) -> x é gordo.
-  .: q(x) -> t(x) se x faz dieta, então engorda.
+  dieta(x) -> x faz dieta.
+  comev(x) -> x consome verduras.
+  comep(x) -> x consome peixe.
+  gordo(x) -> x é gordo.
+  .: dieta(x) -> gordo(x) se x faz dieta, então engorda.
   
-  Para todo x, p(x) então q(x);
-  Para todo d, r(d);
-  Para todo eb, s(eb);
-  t(eb);
-  .: q(x) -> t(x).
+  Para todo x, dieta(x);
+  Para todo e, comev(e);
+  Para todo b, comep(b);
+  gordo(e) ^ gordo(b);
+  .: dieta(x) -> gordo(x).
   
-  (1) p(x) - proposição
-  (2) q(x) - proposição
-  (3) p(x) -> q(x) - premissa
-  (4) r(x) - proposição
-  (5) s(x) - proposição
-  (6) t(x) - proposição
-  (7) s(x) ^ t(x) - conjunção em (5) e (6)
-  (8) r(x) ^ (s(x) ^ t(x)) - conjunção em (7) e (4)
-  (9) 
+  (1) dieta(x) - proposição
+  (2) comev(x) - proposição
+  (3) comep(x) - proposição
+  (4) gordo(x) - proposição
+  (5) comev(x) ^ comep(x) - conjunção em (2) e (3)
+  (6) gordo(x) ^ comev(x) ^ comep(x) - conjunção (4) e (5)
+  -------------------------------------------------------------------------------------------------
+  gordo(elefante) ^ gordo(baleia) ^ comev(elefante) ^ comep(baleia) ^ comev(pessoa) ^ comep(pessoa)
+  _________________________________________________________________________________________________
+  .: dieta(pessoa) -> gordo(pessoa)
   
+  Não é possível afirmar esta conclusão, pois gordo(elefante) ^ gordo(baleia) ^ comev(elefante) ^ comep(baleia) ^ comev(pessoa) ^ comep(pessoa) -> gordo(pessoa)
+  não é uma tautologia. Portanto este argumento é uma falácia.
+  
+  Fazendo uma prova pela contrapositiva(~q -> ~p):
+  consulta: entao(\+gordo(pessoa), \+ (gordo(elefante); gordo(baleia); comev(elefante); comep(baleia); comev(pessoa); comep(pessoa)))
 */
